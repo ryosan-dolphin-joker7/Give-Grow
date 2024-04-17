54d4b11e07e5ad940d1b44475489324f403f1ab3
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-04-17 23:13:32 +0900

    APIキーを表示するコードを追加

--- services/meigen_source.py
+++ services/meigen_source.py
@@ -12,14 +12,14 @@ if not os.getenv("CI"):
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 CUSTOM_SEARCH_ENGINE_ID = os.getenv('CUSTOM_SEARCH_ENGINE_ID')
 
-st.write(GOOGLE_API_KEY)
-st.write(CUSTOM_SEARCH_ENGINE_ID)
 
 def fetch_image_url(keyword, add_terms="画像"):
     # 検索キーワードに追加の用語を組み込む
     full_keyword = f"{keyword} {add_terms}"
     print(f"Fetching image URL for keyword: {full_keyword}")
     service = build("customsearch", "v1", developerKey=GOOGLE_API_KEY)
+    st.write(GOOGLE_API_KEY)
+    st.write(CUSTOM_SEARCH_ENGINE_ID)
     try:
         result = service.cse().list(
             q=full_keyword,  # 更新された検索キーワード
