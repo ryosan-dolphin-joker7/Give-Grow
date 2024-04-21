bc868df5d8516582ae1f4b43ab2b8c67403b53ce
emorinrin <yuya.ctd@gmail.com>
2024-04-21 14:57:35 +0900

    文言・説明を調整。

--- services/edited_image.py
+++ services/edited_image.py
@@ -55,7 +55,7 @@ def edited_image(selected_quote, selected_author):
     available_images = [f for f in os.listdir(img_folder_path) if os.path.isfile(os.path.join(img_folder_path, f))]
     st.markdown('##')
     st.subheader("③ 名言を入れ込む画像を用意してください")
-    uploaded_file = st.file_uploader("③-A アップロード ※任意の画像がない場合はUP不要", type=['png', 'jpg', 'jpeg'], key=f"file_uploader_{selected_quote}")
+    uploaded_file = st.file_uploader("③-A アップロード  ⚠️任意の画像がない場合はUP不要", type=['png', 'jpg', 'jpeg'], key=f"file_uploader_{selected_quote}")
 
     image = None
     if uploaded_file is not None:
@@ -63,7 +63,7 @@ def edited_image(selected_quote, selected_author):
         st.session_state['image'] = image
         st.session_state['text_added'] = False
     else:
-        selected_image_file = st.selectbox('③-B テンプレートから選択 ※オススメ✨', available_images, key=f"image_{selected_quote}")
+        selected_image_file = st.selectbox('③-B テンプレートから選択  ✨オススメ', available_images, key=f"image_{selected_quote}")
         st.markdown('##')
         if selected_image_file:
             image_path = os.path.join(img_folder_path, selected_image_file)
@@ -81,7 +81,7 @@ def edited_image(selected_quote, selected_author):
             font_name = st.selectbox("フォントを選択", available_fonts, index=3)
 
             # 名言の入力
-            quote_text = st.text_input("名言の調整 ※スペースを空けると改行できます", selected_quote)
+            quote_text = st.text_input("名言の調整 ❔スペースを空けると改行できます", selected_quote)
             quote_position_x = st.slider("名言のX座標を入力", 0, 500, 70)
             quote_position_y = st.slider("名言のY座標を入力", 0, 500, 60)
             quote_font_size = st.number_input("名言のフォントサイズを入力", value=35, min_value=1, key='quote_font_size')
@@ -105,4 +105,4 @@ def edited_image(selected_quote, selected_author):
                 image_with_text = add_text_to_image(image_with_text, author_text, (author_position_x, author_position_y), font_name, author_font_size, author_text_color, author_max_width)
                 st.session_state['image_with_text'] = image_with_text
                 st.session_state['text_added'] = True
-                st.image(image_with_text, caption='画像が生成されました🧙‍♀️')
+                st.image(image_with_text, caption='🧙‍♀️画像が生成されました')
